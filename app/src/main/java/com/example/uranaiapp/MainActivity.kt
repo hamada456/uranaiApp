472b6b7e6f995a0c2a99776baa7c839f3334286d
hamada456 <73261938+hamada456@users.noreply.github.com>
2021-11-26 11:38:31 +0900

    完成

--- app/src/main/java/com/example/uranaiapp/MainActivity.kt
+++ app/src/main/java/com/example/uranaiapp/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.uranaiapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var tv:TextView = findViewById(R.id.uranaiText)
+        var btn:Button = findViewById(R.id.uranaiBtn)
+
+        btn.setOnClickListener {
+            //配列を用意
+            var uranai = arrayOf("大吉","中吉","小吉","凶")
+            //tv.text = "大吉"
+
+            //乱数、ランダムな数字(0123)
+            var num = Random.nextInt(uranai.count())
+
+            //numを文字列に変換しないとエラー
+            //tv.text = num.toString()
+
+            //表示
+            tv.text = uranai.get(num)
+
+        }
     }
 }
